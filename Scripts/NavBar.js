1aedfd12a0f1485d5242b830eeed180ae430bade
AnAmusingGhost <cflore63@depaul.edu>
2025-08-20 20:48:59 -0500

    added hamburger function in the NavBar.js

--- Scripts/NavBar.js
+++ Scripts/NavBar.js
@@ -43,5 +43,19 @@ function setActiveNavlink(){
     });
 }
 
+function setupHamburgerMenu() {
+    const hamburger = document.querySelector('.hamburger');
+    const navMenu = document.querySelector('.links-container');
 
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('active');
+        navMenu.classList.toggle('active');
+    });
 
+    document.querySelectorAll('.nav-link').forEach(link => {
+        link.addEventListener('click', () => {
+            hamburger.classList.remove('active');
+            navMenu.classList.remove('active');
+        });
+    });
+}
